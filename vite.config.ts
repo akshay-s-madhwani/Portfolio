05ba786eefe408c485f5da3a33c3b87ca1b3f4a7
Akshay S Madhwani <asmfreelancer99@gmail.com>
2026-03-18 23:51:14 +0530

    built files with fixed assets issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { inspectAttr } from 'kimi-plugin-inspect-react';
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio',
-  plugins: [inspectAttr(), react()],
+  base: '/Portfolio/',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
